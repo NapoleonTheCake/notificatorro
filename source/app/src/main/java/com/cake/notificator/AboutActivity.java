558a08592329d7d5bdb62a99690fa6f180e3d843
Dmitry <contactwithdmitry@gmail.com>
2017-04-22 15:32:12 +0500

    quick commit

--- source/app/src/main/java/com/cake/notificator/AboutActivity.java
+++ source/app/src/main/java/com/cake/notificator/AboutActivity.java
@@ -1,11 +1,19 @@
 package com.cake.notificator;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -32,5 +40,36 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    //show dialog.
+    public void onClick_CoffeeCup(View view) {
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View promptDelay = layoutInflater.inflate(R.layout.prompt_about_coffee, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setView(promptDelay);
+
+        alertDialogBuilder
+                .setCancelable(true);
+
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    //open project page.
+    public void onClick_CoffeeCup_Page(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://github.com/NapoleonTheCake/notificatorro"));
+        startActivity(Intent.createChooser(browserIntent, getString(R.string.ui_OpenPage)));
+    }
+
+    //send me an email.
+    public void onClick_CoffeeCup_Mailme(View view) {
+        Intent mailIntent = new Intent(Intent.ACTION_SEND);
+        mailIntent.setType("message/rfc822")
+                .putExtra(Intent.EXTRA_EMAIL, new String[] { "contactwithdmitry@gmail.com" })
+                .putExtra(Intent.EXTRA_SUBJECT, "Notificatorro: ");
+        startActivity(Intent.createChooser(mailIntent, getString(R.string.ui_SendEmail)));
     }
 }
